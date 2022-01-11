08953dce8fa206b12556f27fb5989ab18904b8e2
BhavyaBibi <bhavyabibi007@gmail.com>
2022-01-11 16:52:11 +0400

    some changes added in the settings

--- averagecode.c
+++ averagecode.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
+int main()
 {
     printf("Enter the 3 numbers");
     float num1, num2, num3, avg;
     scanf("%f%f%f", &num1, &num2, &num3);
     avg = (num1 + num2 + num3) / 3;
+    printf("Result is %f ", num1, num2, num3, avg);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
