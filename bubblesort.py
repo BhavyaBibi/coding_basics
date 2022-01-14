3ea2a6371929be944a67f0cc36d7f9bf4f7f42d5
BhavyaBibi <bhavyabibi007@gmail.com>
2022-01-14 16:27:26 +0400

    one of the sorting mechanism added in the coding

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+def bubble_sort(nums):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+
+                swapped = True
+
+
+# Verify if it works
+random_list_of_nums = [5, 2, 1, 8, 4]
+bubble_sort(random_list_of_nums)
+print(random_list_of_nums)
