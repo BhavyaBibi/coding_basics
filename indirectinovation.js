3ceace01dc152694d3738d1eab15cd97d7e0d8d7
BhavyaBibi <bhavyabibi007@gmail.com>
2022-02-07 17:25:00 +0400

    that is added successfully in the section

---
+++ indirectinovation.js
@@ -0,0 +1,13 @@
+function getBrand(prefix) {
+    console.log(prefix + this.brand);
+}
+
+let honda = {
+    brand: 'Honda'
+};
+let audi = {
+    brand: 'Audi'
+};
+
+getBrand.call(honda, "It's a ");
+getBrand.call(audi, "It's an ");
