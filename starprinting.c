55d815eb411ebc99c6168a962ad1ffe3cc99e958
BhavyaBibi <bhavyabibi007@gmail.com>
2022-04-25 15:07:45 +0400

    the all changes upload in the section

---
+++ starprinting.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, num;
+
+    printf("enter the number: ");
+    scanf("%d\n", &num);
+    for(i=1; i<=num; i++)
+    {
+        for(j=1; j<=num; j++)
+        {
+            printf("*");
+
+        
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
