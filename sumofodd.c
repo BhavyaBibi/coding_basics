6b2d0f474197512e471937dc73872bbbf2c0ee21
BhavyaBibi <bhavyabibi007@gmail.com>
2022-04-27 10:04:12 +0400

    the value added in the number

--- sumofodd.c
+++ sumofodd.c
@@ -3,17 +3,16 @@
 
 int main()
 {
-int i,limit,sum=0;
-printf("enter the limit");
-scanf("%d",&limit);
-for (i=1;i<=limit;i++)
-{
- if(i%2!=0){  
-     printf("%d",i);
-     sum =sum + i;21
-
-
- }    
-}
+    int i, limit, sum = 0;
+    printf("enter the limit");
+    scanf("%d", &limit);
+    for (i = 1; i <= limit; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d", i);
+            sum = sum + i;
+        }
+    }
     return 0;
 }
