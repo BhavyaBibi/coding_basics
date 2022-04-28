f6593e311b1958dbee1a234c259b632ad9881e98
BhavyaBibi <bhavyabibi007@gmail.com>
2022-04-28 11:11:32 +0400

    array of values added

---
+++ arrayfirst.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5];
+    int i;
+    printf("enter the values");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("enterd the value is :%d ", a[0]);
+    return 0;
+}
